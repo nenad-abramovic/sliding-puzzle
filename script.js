e5b006456af6b4ee1a7899a03ce4b45b43ab8e94
Ненад Абрамовић <nenadabramovic@outlook.com>
2020-03-01 00:30:38 +0100

    Minor changes

--- script.js
+++ script.js
@@ -14,8 +14,10 @@ for (let i = 0; i < 4; i++) {
   }
 }
 
-board[3][3].id = 'f-empty';
-board[3][3].textContent = '';
+let id = 'f-empty';
+let textContent = '';
+board[3][3].id = id;
+board[3][3].textContent = textContent;
 
 let emptyField = {
   row: 3,
@@ -23,49 +25,63 @@ let emptyField = {
 };
 
 const handleKeyUp = (e) => {
-  let tmp = board[emptyField.row][emptyField.col].textContent;
-
   switch (e.keyCode) {
     case 37:
       if (emptyField.col === 3) {
         break;
       }
-      board[emptyField.row][emptyField.col].textContent = board[emptyField.row][emptyField.col + 1].textContent;
 
-      board[emptyField.row][emptyField.col + 1].textContent = tmp;
+      board[emptyField.row][emptyField.col].textContent = board[emptyField.row][emptyField.col + 1].textContent;
+      board[emptyField.row][emptyField.col].id = '';
+
+      board[emptyField.row][emptyField.col + 1].textContent = textContent;
+      board[emptyField.row][emptyField.col + 1].id = id;
 
       emptyField.col++;
       break;
+
     case 38:
       if (emptyField.row === 3) {
         break;
       }
-      board[emptyField.row][emptyField.col].textContent = board[emptyField.row + 1][emptyField.col].textContent;
 
-      board[emptyField.row + 1][emptyField.col].textContent = tmp;
+      board[emptyField.row][emptyField.col].textContent = board[emptyField.row + 1][emptyField.col].textContent;
+      board[emptyField.row][emptyField.col].id = '';
+
+      board[emptyField.row + 1][emptyField.col].textContent = textContent;
+      board[emptyField.row + 1][emptyField.col].id = id;
 
       emptyField.row++;
       break;
+
     case 39:
       if (emptyField.col === 0) {
         break;
       }
-      board[emptyField.row][emptyField.col].textContent = board[emptyField.row][emptyField.col - 1].textContent;
 
-      board[emptyField.row][emptyField.col - 1].textContent = tmp;
+      board[emptyField.row][emptyField.col].textContent = board[emptyField.row][emptyField.col - 1].textContent;
+      board[emptyField.row][emptyField.col].id = '';
+
+      board[emptyField.row][emptyField.col - 1].textContent = textContent;
+      board[emptyField.row][emptyField.col - 1].id = id;
 
       emptyField.col--;
       break;
+
     case 40:
       if (emptyField.row === 0) {
         break;
       }
-      board[emptyField.row][emptyField.col].textContent = board[emptyField.row - 1][emptyField.col].textContent;
 
-      board[emptyField.row - 1][emptyField.col].textContent = tmp;
+      board[emptyField.row][emptyField.col].textContent = board[emptyField.row - 1][emptyField.col].textContent;
+      board[emptyField.row][emptyField.col].id = '';
+
+      board[emptyField.row - 1][emptyField.col].textContent = textContent;
+      board[emptyField.row - 1][emptyField.col].id = id;
 
       emptyField.row--;
       break;
+
     default:
       break;
   }
