0572a7fe02905574e8e8805d633c09984dd97c70
Ненад Абрамовић <nenadabramovic@outlook.com>
2020-03-01 01:21:12 +0100

    Minor changes

--- script.js
+++ script.js
@@ -1,41 +1,86 @@
 const container = document.getElementById('container');
+const newGameBtn = document.getElementById('new-game-btn');
+const moveCount = document.getElementById('move-count');
 
 const board = [];
+let totalMoves;
+resetMoveCount();
+startNewGame();
+let emptyField = { row: 3, col: 3 };
 
-for (let i = 0; i < 4; i++) {
-  board.push([]);
 
-  for (let j = 1; j < 5; j++) {
-    let field = document.createElement('div');
-    field.textContent = i * 4 + j;
+window.addEventListener('keyup', handleKeyUp);
+newGameBtn.addEventListener('click', function () {
+  clearDOMContainer();
+  startNewGame();
+});
 
-    board[i].push(field);
-    container.appendChild(field);
+function createBoard() {
+  for (let i = 0; i < 4; i++) {
+    board.push([]);
+
+    for (let j = 1; j < 5; j++) {
+      let field = document.createElement('div');
+      field.textContent = i * 4 + j;
+      board[i].push(field);
+    }
+  }
+
+  board[3][3].id = 'f-empty';
+  board[3][3].textContent = '';
+}
+
+function shuffleBoard() {
+  for (let i = 14; i > -1; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let tmp = board[Math.floor(i / 4)][i % 4];
+    board[Math.floor(i / 4)][i % 4] = board[Math.floor(j / 4)][j % 4];
+    board[Math.floor(j / 4)][j % 4] = tmp;
   }
 }
 
-let id = 'f-empty';
-let textContent = '';
-board[3][3].id = id;
-board[3][3].textContent = textContent;
+function addBoardToDOMContainer() {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      container.appendChild(board[i][j]);
+    }
+  }
+}
 
-let emptyField = {
-  row: 3,
-  col: 3
-};
+function clearDOMContainer() {
+  container.innerHTML = '';
+}
 
-const handleKeyUp = (e) => {
+function startNewGame() {
+  resetMoveCount();
+  createBoard();
+  shuffleBoard();
+  addBoardToDOMContainer();
+}
+
+function updateMoveCount() {
+  totalMoves++;
+  moveCount.textContent = totalMoves;
+}
+
+function resetMoveCount() {
+  totalMoves = 0;
+  moveCount.textContent = totalMoves;
+}
+
+function handleKeyUp(e) {
   switch (e.keyCode) {
     case 37:
       if (emptyField.col === 3) {
         break;
       }
 
+      updateMoveCount();
       board[emptyField.row][emptyField.col].textContent = board[emptyField.row][emptyField.col + 1].textContent;
       board[emptyField.row][emptyField.col].id = '';
 
-      board[emptyField.row][emptyField.col + 1].textContent = textContent;
-      board[emptyField.row][emptyField.col + 1].id = id;
+      board[emptyField.row][emptyField.col + 1].textContent = '';
+      board[emptyField.row][emptyField.col + 1].id = 'f-empty';
 
       emptyField.col++;
       break;
@@ -45,11 +90,12 @@ const handleKeyUp = (e) => {
         break;
       }
 
+      updateMoveCount();
       board[emptyField.row][emptyField.col].textContent = board[emptyField.row + 1][emptyField.col].textContent;
       board[emptyField.row][emptyField.col].id = '';
 
-      board[emptyField.row + 1][emptyField.col].textContent = textContent;
-      board[emptyField.row + 1][emptyField.col].id = id;
+      board[emptyField.row + 1][emptyField.col].textContent = '';
+      board[emptyField.row + 1][emptyField.col].id = 'f-empty';
 
       emptyField.row++;
       break;
@@ -59,11 +105,12 @@ const handleKeyUp = (e) => {
         break;
       }
 
+      updateMoveCount();
       board[emptyField.row][emptyField.col].textContent = board[emptyField.row][emptyField.col - 1].textContent;
       board[emptyField.row][emptyField.col].id = '';
 
-      board[emptyField.row][emptyField.col - 1].textContent = textContent;
-      board[emptyField.row][emptyField.col - 1].id = id;
+      board[emptyField.row][emptyField.col - 1].textContent = '';
+      board[emptyField.row][emptyField.col - 1].id = 'f-empty';
 
       emptyField.col--;
       break;
@@ -73,11 +120,12 @@ const handleKeyUp = (e) => {
         break;
       }
 
+      updateMoveCount();
       board[emptyField.row][emptyField.col].textContent = board[emptyField.row - 1][emptyField.col].textContent;
       board[emptyField.row][emptyField.col].id = '';
 
-      board[emptyField.row - 1][emptyField.col].textContent = textContent;
-      board[emptyField.row - 1][emptyField.col].id = id;
+      board[emptyField.row - 1][emptyField.col].textContent = '';
+      board[emptyField.row - 1][emptyField.col].id = 'f-empty';
 
       emptyField.row--;
       break;
@@ -86,6 +134,3 @@ const handleKeyUp = (e) => {
       break;
   }
 }
-
-window.addEventListener('keyup', handleKeyUp);
-
